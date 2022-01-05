79483856e0f6d6f9fa245c143c730c6342105adb
Aadi Poddar <aadipoddarmail@gmail.com>
2022-01-05 13:22:22 +0530

    Updated Premake

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,9 @@
 project "Glad"
     kind "StaticLib"
     language "C"
-    
+    cppdialect "C++20"
+    staticruntime "on"
+
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
@@ -12,15 +14,18 @@ project "Glad"
         "src/glad.c"
     }
 
-	includedirs
-	{
-		"include"
-	}
-    
+    includedirs
+    {
+        "include"
+    }
+
     filter "system:windows"
         systemversion "latest"
-        cppdialect "C++20"
-        staticruntime "On"
-        
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
